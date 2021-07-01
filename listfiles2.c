1a0acd9b0f641ec2afb977c8db21698b9df8bdca
AR <63246629+mohdrab01@users.noreply.github.com>
2021-07-01 14:49:07 +0530

    Create listfiles2.c

---
+++ listfiles2.c
@@ -0,0 +1,21 @@
+/*
+Q. List the files in current directory -- method 2
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]){
+        char d[50];
+        if (argc==2){
+                bzero(d,sizeof(d));
+                strcat(d,"ls ");
+                strcat(d,argv[1]);
+                system(d);
+        }
+        else{
+                printf("\nInvalid No. of Arguments");
+        }
+        return 0;
+}
